5272bc2be206a3e8d694e00c28e25c721868e49d
andrewmherren <andrew.herren@gmail.com>
2017-04-07 17:50:11 -0700

    commented out routes that dont exist yet

--- http/routes.js
+++ http/routes.js
@@ -8,16 +8,16 @@ exports.routes = function (app) {
   /**
   * Question API routes
   */
-  app.get('/question', questionController.get)
-  app.get('/question/:id', questionController.getOne)
-  app.post('/question', questionController.post)
-  app.post('/question/_search', questionController.search)
-  app.put('/question/:id', questionController.put)
-  app.delete('/question/:id', questionController.delete)
+  // app.get('/question', questionController.get)
+  // app.get('/question/:id', questionController.getOne)
+  // app.post('/question', questionController.post)
+  // app.post('/question/_search', questionController.search)
+  // app.put('/question/:id', questionController.put)
+  // app.delete('/question/:id', questionController.delete)
 
   /**
   * Answer API routes
   */
   app.get('/answer/:id', answerController.getOne)
-  app.delete('/answer/:id', answerController.delete)
+  //app.delete('/answer/:id', answerController.delete)
 }
